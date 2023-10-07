52701e8265923dd6d8f4924c7699502b83a1643d
AlishaKausheen <alishakausheen@gmail.com>
2023-10-07 00:32:21 -0700

    collision function added

--- space.js
+++ space.js
@@ -139,3 +139,11 @@ function createAlien() {
   }
   alienCount = alienArray.length;
 }
+function detectCollision(a, b) {
+  return (
+    a.x < b.y + b.width && //a's top left corner doesn't reach b's top right corner
+    a.x + a.width > b.x && //a's top right corner passes b's top left corner
+    a.y < b.y + b.height && //a's top left corner doesnt reach bottom left corner
+    a.y + a.height > b.y
+  ); //a's bottom left corner passes b's top left corner
+}
